73fe765ad51d456d1ab9d0570283bb2a5c75cc5e
Jenral <dennis_otu@live.com>
2021-05-09 11:40:16 +0000

    updated blog models + made migrations + created superuser

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-posts = [
-    {
-        'title': 'first post title',
-        'author': 'jenral',
-        'date': 'may 2, 2020',
-        'content': 'first post content'
-    },
-
-    {
-        'title': 'second post title',
-        'author': 'kweku',
-        'date': 'may 4, 2020',
-        'content': 'second post content'
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all(),
         'pageTitle': 'Home Page'
     }
     return render(request, 'blog/home.html', context)
