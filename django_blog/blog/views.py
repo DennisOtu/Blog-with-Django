3f172f708747a7d4948f8de697661cabe0d3fd3f
Jenral <dennis_otu@live.com>
2021-05-12 01:26:25 +0000

    added new post form + post detail view

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.views.generic import DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 
 def home(request):
@@ -9,6 +12,17 @@ def home(request):
         'pageTitle': 'Home Page'
     }
     return render(request, 'blog/home.html', context)
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 def about(request):
     return render(request, 'blog/about.html', {'pageTitle': 'About Page'})
